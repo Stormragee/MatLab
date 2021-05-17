ad901ff350b9a9e32c949156c493417b95f9ae1a
Mariusz <mariusz.majzner@gmail.com>
2021-05-17 13:18:04 +0200

    Final

--- ode.m
+++ ode.m
@@ -1,8 +1,9 @@
 function ode
-t=0:0.001:5;   % time scalex
+t=0:0.00001:3;   % time scalex
 initial_t = 10;
-initial_q = 0;
-[g,x] =ode23( @(g,x) odefun(g,x), t, [initial_t, initial_q]);
+initial_q = 10000;
+
+[q,x] =ode23( @(g,x) odefun(g,x), t, [initial_t, initial_q]);
 
 
 function dqdt = odefun(t,q)
@@ -10,9 +11,9 @@ function dqdt = odefun(t,q)
     x = q(1);
     v = q(2);
     global m0 s k;
-    m0 = 10.0; % kg
-    s = 10; % parametr s w sekundach.
-    k = 1000; % parametr 
+    m0 = 30.0; % kg
+    s = 1; % parametr s w sekundach.
+    k = 10; % parametr 
     m = m0 * exp(-t/s);
     F = -k * x;
     
@@ -24,9 +25,15 @@ function dqdt = odefun(t,q)
     dqdt(2) = a;
 end
 
+%Filtrowanie wyniku
+idx = x(:,2)>0;
+PosVals = x(idx,2);
+%plot(q,x(:,2));
 
-plot(g,x);
+
+plot(q(1:length(PosVals)),PosVals);
+%plot(g,x);
 xlabel('t'); ylabel('v');
 drawnow;
-%plot(x(:,1),x(:,2));
+
 end
