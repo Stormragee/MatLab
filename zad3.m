6787f37dcaa21a31fb78f38b194547a7cf07d1da
Mariusz <mariusz.majzner@gmail.com>
2021-06-07 13:17:06 +0200

    Zadania na zajeciach

---
+++ zad3.m
@@ -0,0 +1,19 @@
+
+N = 5;
+A = rand(N);
+S = A;
+% Dla podanego rozmiaru losowa macierz i ile razy można przypadkow uzyskac
+% przesuwa o 1 o 1 o 1 o 1 
+% sprawdzanie wyznaczników czy poprawnie się przemieszcza
+for i = 1:N-1
+    S = circshift(S,[0,1]);
+    %if det(A)) ~= det(S) Przez procesy obliczeniowe wynik ten jest
+    %fałszem! ans = 5.551115123125783e-17
+    if floor(det(A)) ~= floor(det(S))
+        disp("Sa rozne!")
+        det(A)
+        det(S)
+        det(A) - det(S)
+        break
+    end
+end
