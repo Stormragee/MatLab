6ce6ea8899818735de75a67521dd3073e4c8a78a
Stormragee <62036455+Stormragee@users.noreply.github.com>
2021-06-07 13:39:47 +0200

    Update sinus.m

--- sinus.m
+++ sinus.m
@@ -1,14 +1,12 @@
 f = @(x) sind(x);
-
 x = 0:360;
 y = 0:30:360;
-z = [0:30:360];
 figure(1);
 clf;
 plot(x,f(x))
+plot(y,f(y), 'o','markersize',22,'markerfacecolor','y','markeredgecolor','r')
+text(z-5,f(z),string(z),'fontsize',7)
 hold on;
-plot(y,f(y), 'or')
-text(z,f(z),string(z))
 xlim([0,360]);
 grid minor;
 grid on;
